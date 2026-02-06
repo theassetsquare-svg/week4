34946678e688270e5f40c28e514e715c983a52fa
theassetsquare <theassetsquare@gmail.com>
2026-02-06 14:26:17 +0000

    Support GitHub and Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/week4/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/week4/' : '/',
   plugins: [react()],
-})
+}))
